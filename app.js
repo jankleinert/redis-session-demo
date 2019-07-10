85a8ac91cd132cac7edabb94b976783398fb523b
Jan Kleinert <jan.kleinert@redhat.com>
2019-07-10 18:13:12 -0400

    Removed session-related code to create the base version of the demo

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const uuid = require('uuid/v4')
-const session = require('express-session');
-const redis = require('redis');
-const redisStore = require('connect-redis')(session);
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
-
-const redisClient = redis.createClient();
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-redisClient.on('error', (err) => {
-  console.log('Redis error: ', err);
-});
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  genid: (req) => {
-    return uuid()
-  },
-  store: new redisStore({ host: 'localhost', port: 6379, client: redisClient }),
-  name: '_redisDemo', 
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  cookie: { secure: false, maxAge: 60000 }, // Set to secure:false and expire in 1 minute for demo purposes
-  saveUninitialized: true
-}))
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,22 @@ const router = express.Router();
 const request = require('request');
 
 /* GET request for home page */
-router.get('/', function(req, res, next) {
-  var expireTime = req.session.cookie.maxAge / 1000; 
-  res.render('index', { sessionID: req.sessionID, sessionExpireTime: expireTime, beersViewed: req.session.views, beerName: null, beerStyle: null, error: null });
+router.get('/', function(req, res, next) { 
+  res.render('index', { beerName: null, beerStyle: null, error: null });
 });
 
 
 router.post('/', function (req, res) {
   request('https://www.craftbeernamegenerator.com/api/api.php?type=trained', function (err, response, body) {
-    if (req.session.views) {
-      req.session.views++
-    } else {
-        req.session.views = 1
-    }
-    var expireTime = req.session.cookie.maxAge / 1000;   
-    
     if(err){
-      res.render('index', { sessionID: req.sessionID, sessionExpireTime: expireTime, beersViewed: req.session.views, beerName: null, beerStyle: null, error: 'Error, please try again'});
+      res.render('index', { beerName: null, beerStyle: null, error: 'Error, please try again'});
     } else {
       var beerInfo = JSON.parse(body)
 
       if(beerInfo.status != 200){
-        res.render('index', { sessionID: req.sessionID, sessionExpireTime: expireTime, beersViewed: req.session.views, beerName: null, beerStyle: null, error: 'Error, please try again'});
+        res.render('index', { beerName: null, beerStyle: null, error: 'Error, please try again'});
       } else {
-        res.render('index', { sessionID: req.sessionID, sessionExpireTime: expireTime, beersViewed: req.session.views, beerName: beerInfo.data.name, beerStyle: beerInfo.data.style, error: null});
+        res.render('index', { beerName: beerInfo.data.name, beerStyle: beerInfo.data.style, error: null});
       }
     }
   });
